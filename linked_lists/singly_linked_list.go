c6ca8f123df49238d5ba98c87715110b3eb42afd
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2023-11-05 15:24:23 +0000

    Singly linked list with few more methods and refactoring

--- linked_lists/singly_linked_list.go
+++ linked_lists/singly_linked_list.go
@@ -1,24 +1,34 @@
 package linked_lists
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
+// SNode represents a singly linked list node, with only reference to the next node
 type SNode struct {
 	val  int
 	Next *SNode
 }
 
+// SinglyLinkedList represents a singly linked list with its head node
 type SinglyLinkedList struct {
 	head *SNode
 }
 
+// NewSinglyLinkedListFromArray creates a singly linked list from an array
 func NewSinglyLinkedListFromArray(arr []int) *SinglyLinkedList {
+	sll := &SinglyLinkedList{head: nil}
 	if len(arr) == 0 {
-		return nil
+		return sll
 	}
 
 	// get the first element from the array and create the head node
-	head := &SNode{val: arr[0]}
+	sll.head = &SNode{val: arr[0]}
 
 	// initialise the previous element to link the other elements, with the first item being the head
-	prev := head
+	prev := sll.head
 
 	// for each other element it creates a node
 	for _, val := range arr[1:] {
@@ -28,9 +38,28 @@ func NewSinglyLinkedListFromArray(arr []int) *SinglyLinkedList {
 	}
 
 	// returns the singly linked list with the head node
-	return &SinglyLinkedList{head: head}
+	return sll
 }
 
+// DeleteHead deletes the head of the list
+func (sll *SinglyLinkedList) DeleteHead() error {
+	if sll.head == nil {
+		return errors.New("empty list, can't remove first element")
+	}
+
+	sll.head = sll.head.Next // Move the head to the next element
+	return nil
+}
+
+// Prepend add to beginning of the list, setting a new head
+func (sll *SinglyLinkedList) Prepend(val int) {
+	// creates the new node and set the next to the current head
+	n := &SNode{val: val, Next: sll.head}
+	// set the head to the new node
+	sll.head = n
+}
+
+// Append add to the end of the list
 func (sll *SinglyLinkedList) Append(val int) {
 	n := &SNode{val: val}
 
@@ -47,6 +76,41 @@ func (sll *SinglyLinkedList) Append(val int) {
 	current.Next = n
 }
 
+// Delete all matching values from the list
+func (sll *SinglyLinkedList) Delete(val int) error {
+	if sll.head == nil {
+		return errors.New("empty list, can't delete")
+	}
+
+	// Remove all occurrences from the head first
+	for sll.head != nil && sll.head.val == val {
+		sll.head = sll.head.Next
+	}
+
+	// If the list became empty after removing the head nodes
+	if sll.head == nil {
+		return nil
+	}
+
+	current := sll.head
+	prev := current
+
+	for current != nil {
+		if current.val == val {
+			// skip the current node to remove it
+			prev.Next = current.Next
+			// do not update prev if we removed the current
+		} else {
+			// only update prev if we didn't remove the current
+			prev = current
+		}
+		current = current.Next
+	}
+
+	return nil
+}
+
+// ToArray converts the singly linked list to array
 func (sll *SinglyLinkedList) ToArray() []int {
 	if sll.head == nil {
 		return []int{}
@@ -60,4 +124,20 @@ func (sll *SinglyLinkedList) ToArray() []int {
 	}
 
 	return output
+}
+
+// Display just returns the string representation of the singly linked list
+func (sll *SinglyLinkedList) Display() string {
+	if sll.head == nil {
+		return "empty list"
+	}
+
+	var output []string
+	current := sll.head
+	for current != nil {
+		output = append(output, strconv.Itoa(current.val))
+		current = current.Next
+	}
+
+	return strings.Join(output, "->")
 }
--- main.go
+++ main.go
@@ -2,54 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/renatoaraujo/data-structures-examples-in-go/trees"
+
+	"github.com/renatoaraujo/data-structures-examples-in-go/linked_lists"
 )
 
 func main() {
-	root := &trees.Node{Data: 20}
-	trees.Insert(root, 10)
-	trees.Insert(root, 25)
-	trees.Insert(root, 5)
-	trees.Insert(root, 13)
-	trees.Insert(root, 7)
-	trees.Insert(root, 6)
-	trees.Insert(root, 8)
-	trees.Insert(root, 23)
-	trees.Insert(root, 24)
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	sll := linked_lists.NewSinglyLinkedListFromArray(arr)
+	sll.Append(10)
 
-	//arr := []int{1, 2, 234, 142, 1, 5, 2352, 46, 2346, 3467, 34, 6, 36, 34, 63, 46, 347, 34, 25, 2134, 12}
-	//root := trees.NewBalancedBSTFromSlice(arr)
+	fmt.Println(sll.Display())
+	sll.Append(12)
+	fmt.Println(sll.Display())
+	sll.Prepend(13)
+	fmt.Println(sll.Display())
 
-	//val := 2134
-	//found := root.Search(val)
-	//if found == nil {
-	//	out := fmt.Sprintf("values does not exist in the tree: %d", val)
-	//	fmt.Println(out)
-	//	out = fmt.Sprintf("inserting: %d", val)
-	//	fmt.Println(out)
-	//	trees.Insert(root, val)
-	//
-	//	found = root.Search(val)
-	//	out = fmt.Sprintf("value found after inserting: %d", val)
-	//	fmt.Println(out)
-	//} else {
-	//	out := fmt.Sprintf("already exists in the tree: %d", val)
-	//	fmt.Println(out)
-	//}
-
-	transversal := trees.NewTransversal(root)
-	inOrder := transversal.InOrder()
-	fmt.Println(inOrder)
-
-	preOrder := transversal.PreOrder()
-	fmt.Println(preOrder)
-
-	postOrder := transversal.PostOrder()
-	fmt.Println(postOrder)
-
-	levelOrder := transversal.LevelOrder()
-	fmt.Println(levelOrder)
-
-	reverseLevelOrder := transversal.ReverseLevelOrder()
-	fmt.Println(reverseLevelOrder)
+	err := sll.Delete(5)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(sll.Display())
 }
